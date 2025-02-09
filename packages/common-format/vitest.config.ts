dccba89e91d9f913faed3b110793cafb7a6b5efc
sailist <sailist@outlook.com>
2025-02-10 00:53:28 +0800

    test: fix test

--- packages/common-format/vitest.config.ts
+++ packages/common-format/vitest.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vitest/config';
-
+import path from 'node:path';
 export default defineConfig({
   // Configure Vitest (https://vitest.dev/config/)
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      'common-cursor': path.resolve(__dirname, '../common-cursor/src'),
+    },
   },
 });

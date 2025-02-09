5e98f1543fa3717d7769863e4bb128163806dbd9
sailist <sailist@outlook.com>
2025-02-09 13:08:35 +0800

    add test for insert node

---
+++ tests/query/insertNode.test.ts
@@ -0,0 +1,37 @@
+import { NodeToString } from "../../src/helper";
+import { RangeEditor } from "../../src/editor";
+import { test, expect } from "vitest";
+
+test('insertAfter', () => {
+    const root = document.createElement("div")
+    root.innerHTML = "<p>hello</p>"
+    const editor = new RangeEditor(
+        {
+        },
+        root,
+    );
+
+    const src = document.createElement("b");
+    src.textContent = "hello";
+    editor._insertAfter(src, root.children[0]);
+    const rootStr = NodeToString(root);
+
+    expect(rootStr).toEqual("<div><p>hello</p><b>hello</b></div>");
+});
+
+test('insertBefore', () => {
+    const root = document.createElement("div")
+    root.innerHTML = "<p>hello</p>"
+    const editor = new RangeEditor(
+        {
+        },
+        root,
+    );
+
+    const src = document.createElement("b");
+    src.textContent = "hello";
+    editor._insertBefore(src, root.children[0]);
+    const rootStr = NodeToString(root);
+
+    expect(rootStr).toEqual("<div><b>hello</b><p>hello</p></div>");
+});
